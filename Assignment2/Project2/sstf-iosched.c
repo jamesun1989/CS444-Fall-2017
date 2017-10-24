7e541d2cd05f1555b2bf24cf8e1c5b783540a989
jamesun1989 <jamesun1989@gmail.com>
2017-10-24 11:50:55 -0700

    complete add_request function for C-LOOK algorithm and fix some small bugs.

--- Assignment2/Project2/sstf-iosched.c
+++ Assignment2/Project2/sstf-iosched.c
@@ -3,11 +3,10 @@
 #include <linux/elevator.h>
 #include <linux/init.h>
 #include <linux/module.h>
-#include <linux/slab>
+#include <linux/slab.h>
 
 struct clook_data{
 	struct list_head queue;
-	//sector_t current_position;
 };
 
 static void clook_merged_requests(struct request_queue *q, struct request *rq, struct request *next){
@@ -25,12 +24,12 @@ static int clook_dispatch(struct request_queue *q, int force){
 		list_del_init(&rq->queuelist);
 		elv_dispatch_sort(q,rq);
 
-		if(rq_data_dir(rq) == READ)
-			operation = "R";
+		if(rq_data_dir(rq) == REQ_WRITE)
+			operation = 'W';
 		else
-			operation = "W";
+			operation = 'R';
 
-		printk("CLOOK Dispatch %c %lu\n", operation, blk_rq_pos(rq));
+		printk(KERN_INFO "C-Look Dispatch %c %lu\n", operation, blk_rq_pos(rq));
 
 		return 1;
 	}
@@ -40,52 +39,64 @@ static int clook_dispatch(struct request_queue *q, int force){
 static void clook_add_request(struct request_queue *q, struct request *rq)
 {
         struct clook_data *cd = q->elevator->elevator_data;
-        struct list_head *current = NULL;
+        struct list_head *cur = NULL;
+        struct request *a;
+        char operation;
 
-        list_for_each(current, &cd->queue){
-        	
+        list_for_each(cur, &cd->queue){
+        	a = list_entry(cur, struct request, queuelist);
+        	if(rq_end_sector(rq) < rq_end_sector(a)){
+        		break;
+        	}
         }
 
-        list_add_tail(&rq->queuelist, &cd->queue);
+        list_add_tail(&rq->queuelist, cur);
+
+        if(rq_data_dir(rq) == REQ_WRITE)
+                operation = 'W';
+        else
+                operation = 'R';
+
+        printk(KERN_INFO "C-Look add %c %lu\n", operation, blk_rq_pos(current));
 }
 
 static struct request *
-noop_former_request(struct request_queue *q, struct request *rq)
+clook_former_request(struct request_queue *q, struct request *rq)
 {
-        struct noop_data *nd = q->elevator->elevator_data;
+        struct clook_data *cd = q->elevator->elevator_data;
 
-        if (rq->queuelist.prev == &nd->queue)
+        if (rq->queuelist.prev == &cd->queue)
                 return NULL;
         return list_entry(rq->queuelist.prev, struct request, queuelist);
 }
 
 static struct request *
-noop_latter_request(struct request_queue *q, struct request *rq)
+clook_latter_request(struct request_queue *q, struct request *rq)
 {
-        struct noop_data *nd = q->elevator->elevator_data;
+        struct clook_data *cd = q->elevator->elevator_data;
 
-        if (rq->queuelist.next == &nd->queue)
+        if (rq->queuelist.next == &cd->queue)
                 return NULL;
         return list_entry(rq->queuelist.next, struct request, queuelist);
 }
 
-static int noop_init_queue(struct request_queue *q, struct elevator_type *e)
+static int clook_init_queue(struct request_queue *q, struct elevator_type *e)
 {
-        struct noop_data *nd;
+        struct clook_data *cd;
         struct elevator_queue *eq;
 
         eq = elevator_alloc(q, e);
         if (!eq)
                 return -ENOMEM;
 
-        nd = kmalloc_node(sizeof(*nd), GFP_KERNEL, q->node);
-        if (!nd) {
+        cd = kmalloc_node(sizeof(*cd), GFP_KERNEL, q->node);
+        if (!cd) {
                 kobject_put(&eq->kobj);
                 return -ENOMEM;
         }
-        eq->elevator_data = nd;
+        eq->elevator_data = cd;
 
-        INIT_LIST_HEAD(&nd->queue);
+        INIT_LIST_HEAD(&cd->queue);
 
         spin_lock_irq(q->queue_lock);
         q->elevator = eq;
@@ -93,42 +104,42 @@ static int noop_init_queue(struct request_queue *q, struct elevator_type *e)
         return 0;
 }
 
-static void noop_exit_queue(struct elevator_queue *e)
+static void clook_exit_queue(struct elevator_queue *e)
 {
-        struct noop_data *nd = e->elevator_data;
+        struct clook_data *cd = e->elevator_data;
 
-        BUG_ON(!list_empty(&nd->queue));
-        kfree(nd);
+        BUG_ON(!list_empty(&cd->queue));
+        kfree(cd);
 }
 
-static struct elevator_type elevator_noop = {
+static struct elevator_type elevator_clook = {
         .ops = {
-                .elevator_merge_req_fn          = noop_merged_requests,
-                .elevator_dispatch_fn           = noop_dispatch,
-                .elevator_add_req_fn            = noop_add_request,
-                .elevator_former_req_fn         = noop_former_request,
-                .elevator_latter_req_fn         = noop_latter_request,
-                .elevator_init_fn               = noop_init_queue,
-                .elevator_exit_fn               = noop_exit_queue,
+                .elevator_merge_req_fn          = clook_merged_requests,
+                .elevator_dispatch_fn           = clook_dispatch,
+                .elevator_add_req_fn            = clook_add_request,
+                .elevator_former_req_fn         = clook_former_request,
+                .elevator_latter_req_fn         = clook_latter_request,
+                .elevator_init_fn               = clook_init_queue,
+                .elevator_exit_fn               = clook_exit_queue,
         },
-        .elevator_name = "noop",
+        .elevator_name = "clook",
         .elevator_owner = THIS_MODULE,
 };
 
-static int __init noop_init(void)
+static int __init clook_init(void)
 {
-        return elv_register(&elevator_noop);
+        return elv_register(&elevator_clook);
 }
 
-static void __exit noop_exit(void)
+static void __exit clook_exit(void)
 {
-        elv_unregister(&elevator_noop);
+        elv_unregister(&elevator_clook);
 }
 
-module_init(noop_init);
-module_exit(noop_exit);
+module_init(clook_init);
+module_exit(clook_exit);
 
 
-MODULE_AUTHOR("Jens Axboe");
+MODULE_AUTHOR("Group 35");
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("No-op IO scheduler");
+MODULE_DESCRIPTION("CLOOK IO scheduler");
