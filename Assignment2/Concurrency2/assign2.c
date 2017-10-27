daf178c953116ec819ed7b63fcc8f804549d9e55
jamesun1989 <jamesun1989@gmail.com>
2017-10-26 17:15:05 -0700

    Finished con2 and comments needed to be added

--- Assignment2/Concurrency2/assign2.c
+++ Assignment2/Concurrency2/assign2.c
@@ -1,7 +1,7 @@
 /*
 *CS444 Concurrency 2
 *Xiaoli Sun, ID: 932051695
-*Jaydeep Hemant Rotithor, ID: 
+*Jaydeep Hemant Rotithor, ID: 931862831
 */
 
 
@@ -10,11 +10,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <semaphore.h>
-#include " mt19937ar.c"
+#include <string.h>
 
 #define MAX 5
 
-sem_t fork[MAX];
+//pthread_mutex_t forks[MAX];
+sem_t forks[MAX];
 pthread_mutex_t mutex;
 
 struct philosophor_struct{
@@ -22,16 +23,20 @@ struct philosophor_struct{
 	int philosophor_id;
 };
 
+int philosophor_condition[MAX];
+
 void eat();
 void think();
 void get_forks();
 void put_forks();
-void do();
+void *doit();
 
 void think(struct philosophor_struct *p){
 	int sleep_time = rand() % 20 + 1;
 
-	printf("%s is hungry and waiting for %d seconds\n", p->name, sleep_time);
+	pthread_mutex_lock(&mutex);
+	printf("%s is hungry.\n", p->philosophor_name);
+	pthread_mutex_unlock(&mutex);
 
 	sleep(sleep_time);
 }
@@ -39,11 +44,96 @@ void think(struct philosophor_struct *p){
 void eat(struct  philosophor_struct *p){
 	int sleep_time = rand() % 8 + 2;
 
-	printf("%s is eating noodles for %d seconds\n", p->name, sleep_time);
+	pthread_mutex_lock(&mutex);
+	printf("%s is eating noodles.\n", p->philosophor_name);
+	pthread_mutex_unlock(&mutex);
 
 	sleep(sleep_time);
 }
 
 void get_forks(struct philosophor_struct *p){
+	int left_fork = (p->philosophor_id + 5) % MAX;
+	int right_fork = (p->philosophor_id + 1) % MAX;
+	int sem_value;
+	int forks_check = 1;
+
+	while(forks_check){
+		sem_getvalue(&forks[left_fork], &sem_value);
+
+		if(sem_value == 1){
+			sem_wait(&forks[left_fork]);
+
+			sem_getvalue(&forks[right_fork], &sem_value);
+			if(sem_value == 1){
+				sem_wait(&forks[right_fork]);
+
+				forks_check = 0;
+			}else{
+				sem_post(&forks[left_fork]);
+			}
+		}
+	}
+
+	printf("%s get forks %d and %d.\n", p->philosophor_name, left_fork, right_fork);
+
 	
+}
+
+void put_forks(struct philosophor_struct *p){
+	//philosophor_condition[p->philosophor_id] = 1;
+
+	int left_fork = (p->philosophor_id + 5) % MAX;
+	int right_fork = (p->philosophor_id + 1) % MAX;
+
+	pthread_mutex_lock(&mutex);
+	printf("%s puts down forks %d and %d\n", p->philosophor_name, left_fork, right_fork);
+	pthread_mutex_unlock(&mutex);
+
+	//sem_post(&forks[left_fork]);
+	//sem_post(&forks[right_fork]);
+	sem_post(&forks[right_fork]);
+	sem_post(&forks[left_fork]);
+}
+
+void *doit(void *p){
+	struct philosophor_struct *philo = (struct philosophor_struct *)p;
+
+	while(1){
+		think(philo);
+		get_forks(philo);
+		eat(philo);
+		put_forks(philo);
+	}
+}
+
+int main(){
+	int i;
+
+	pthread_t p_thread[MAX];
+
+	struct philosophor_struct *philo;
+
+	philo = malloc(sizeof(struct philosophor_struct)*5);
+
+	strcpy(philo[0].philosophor_name, "Kevin");
+	strcpy(philo[1].philosophor_name, "Xiaoli");
+	strcpy(philo[2].philosophor_name, "Jaydeep");
+	strcpy(philo[3].philosophor_name, "Batman");
+	strcpy(philo[4].philosophor_name, "Ironman");
+
+	pthread_mutex_init(&mutex, NULL);
+
+	for(i=0;i<MAX;i++){
+		sem_init(&forks[i], 0, 1);
+	}
+
+	for(i=0;i<MAX;i++){
+		philo[i].philosophor_id = i;
+		pthread_create(&p_thread[i], NULL, doit, &philo[i]);
+	}
+
+	for(i=0;i<MAX;i++){
+		pthread_join(p_thread[i], NULL);
+	}
+	return 0;
 }
